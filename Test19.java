122bcbce32dae8c2884c7e195d2eab38060c84fd
soyoung00 <parksy9750@naver.com>
2020-08-13 14:27:13 +0900

    step1

---
+++ Test19.java
@@ -0,0 +1,12 @@
+public class java Test19{
+	public static void main(Stirng[] args {
+		int score=90;
+		String result="";
+
+		if(score>=60)
+			result="pass";
+		else
+			result="fail";
+		System.out.println(result);
+	}
+}
